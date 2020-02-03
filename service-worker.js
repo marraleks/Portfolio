caf416c6c5f5c13bb84b8c6154635a2624e70f4a
Marius Aleksander <marraleks@hotmail.com>
2020-02-03 10:24:28 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.7de15ac6a01e366d8094fc04598adce4.js"
+  "/Portfolio/precache-manifest.7a4b30bffec4fd604093c6560a433868.js"
 );
 
 self.addEventListener('message', (event) => {
