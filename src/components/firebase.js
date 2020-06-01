6cb9dbdfc5125106db0eb065ce2c2c43dd77a9a7
Marius Aleksander Sletten <marraleks@hotmail.com>
2020-06-01 10:16:18 +0200

    firestore fix and overflow

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,16 +1,18 @@
-import firebase from 'firebase'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/firebase-auth";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDDuD9VvedFdkSXbKfIGV9aSmWOnk7cQb4",
-    authDomain: "react-blog-firebase-43de4.firebaseapp.com",
-    databaseURL: "https://react-blog-firebase-43de4.firebaseio.com",
-    projectId: "react-blog-firebase-43de4",
-    storageBucket: "react-blog-firebase-43de4.appspot.com",
-    messagingSenderId: "1020948357847",
-    appId: "1:1020948357847:web:6bedc940343de3ad43c692",
-    measurementId: "G-MBJ5DW61LV"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDDuD9VvedFdkSXbKfIGV9aSmWOnk7cQb4",
+  authDomain: "react-blog-firebase-43de4.firebaseapp.com",
+  databaseURL: "https://react-blog-firebase-43de4.firebaseio.com",
+  projectId: "react-blog-firebase-43de4",
+  storageBucket: "react-blog-firebase-43de4.appspot.com",
+  messagingSenderId: "1020948357847",
+  appId: "1:1020948357847:web:6bedc940343de3ad43c692",
+  measurementId: "G-MBJ5DW61LV",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  export default firebase
+export default firebase;
