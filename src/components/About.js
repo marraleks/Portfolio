162147000fdfc39d6b792d2ce28596b4b10b71e6
Marius Sletten <msletten@norse.digital>
2020-06-04 13:30:29 +0200

    Added overflow-x hidden to main

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,45 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
 
 const About = () => {
-    return(
-        <main>
-            <div className='AboutContainer '>
-            <div className='about animated slideInUp'>
-                <h3 >About me</h3>
-                <h4 >Marius Aleksander Sletten</h4>
-                <div >
-                <p >I'm a UX/UI designer who is extra passionate about Front-end development. It may sound boring to many, but to me there is something special about it. I can sit down for hours forgetting the time just because I want to solve the next problem.</p>
-            </div>
-            </div>
-            <div className='contact animated slideInUp'>
-                <h3>Contact me</h3>
-                <a href="mailto:marraleks@hotmail.com">marraleks@hotmail.com</a>
-                <p>+47 97 17 97 98</p>
-                <h3>Find me on</h3>
-                    <a href='https://www.linkedin.com/in/marius-aleksander-sletten-92aa3716a/' target="_blank" rel="noopener noreferrer">linkedin</a>
-                    <a href='https://github.com/marraleks' target="_blank" rel="noopener noreferrer">github</a>
-            </div>
-            </div>
-        </main>
-    )
-}
+  return (
+    <main>
+      <div className="AboutContainer ">
+        <div className="about animated slideInUp">
+          <h3>About me</h3>
+          <h4>Marius Aleksander Sletten</h4>
+          <div>
+            <p>
+              I'm a UX/UI designer who is extra passionate about Front-end
+              development. It may sound boring to many, but to me there is
+              something special about it. I can sit down for hours forgetting
+              the time just because I want to solve the next problem.
+            </p>
+          </div>
+        </div>
+        <div className="contact animated slideInUp">
+          <h3>Contact me</h3>
+          <a href="mailto:marraleks@hotmail.com">marraleks@hotmail.com</a>
+          <p>+47 97 17 97 98</p>
+          <h3>Find me on</h3>
+          <a
+            href="https://www.linkedin.com/in/marius-aleksander-sletten-92aa3716a/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin
+          </a>
+          <a
+            href="https://github.com/marraleks"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default About
+export default About;
